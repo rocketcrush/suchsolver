334d99970fc3271e74c34f6885a42df3613cdb31
Adem Gürkan Türk <a.gurkanturk@gmail.com>
2017-02-10 22:22:00 +0300

    Add files via upload

---
+++ include/constants.h
@@ -0,0 +1,22 @@
+extern double R;
+extern double gama;
+
+extern double Pr;
+extern double cv;
+extern double cp;
+
+extern double C; //courant number
+extern double physicaldeltat;
+
+extern bool readbackupflag;
+extern int writebackupflag;
+extern bool dimensionalflag;
+extern int wallflag;
+extern bool writeneigborflag;
+extern bool readneigborflag;
+extern bool vtkoutputflag;
+extern bool datoutputflag;
+extern bool heatrateflag;
+
+void getconstants();
+ 
---
+++ include/fluxes.h
@@ -0,0 +1,16 @@
+void W(int);
+
+void Fc1(int);
+
+void Fv1(int);
+
+
+void W3(int, int);
+
+void Fc1new(int);
+
+
+void Fc1new1(int);
+
+
+
